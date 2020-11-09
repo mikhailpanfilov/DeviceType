fc1be7f75bc79bc01f52c0010d62419185da32d0
Mikhail Panfilov <mikhail.panfilov@yahoo.com>
2020-11-09 23:00:15 +0200

    Update DeviceType. iPhone 12 added

--- DeviceType.swift
+++ DeviceType.swift
@@ -17,13 +17,22 @@ struct ScreenSize {
 
 /// Type of device, based on screen size.
 struct DeviceType {
-    let userInterfaceIdiom = UIDevice.current.userInterfaceIdiom
+    
+    /// 5, 5s, 5c, SE
     static let iPhoneSE = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength <= 568.0
-    static let iPhone8 = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 667.0
-    static let iPhone8Plus = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength >= 736.0
-    static let iPhoneX = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 812.0
-    // Aslo iPhone Xr
-    static let iPhoneXsMax = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 896.0
+    /// 6+ 6s 7, 8
+    static let iPhoneWithoutTopNotch = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 667.0
+    /// 6+, 6s+ 7+, 8+
+    static let iPhonePlus = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 736.0
+    /// X, Xs, 11 Pro, 12 Mini
+    static let iPhoneXOr12Mini = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 812.0
+    // Xr, Xs Max, 11, 11 Pro Max
+    static let iPhoneXrOrMax = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 896.0
+    /// 12, 12 Pro
+    static let iPhone12 = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 844.0
+    /// 12 Pro Max
+    static let iPhone12Max = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.maxLength == 926.0
+    
     static let iPhone = UIDevice.current.userInterfaceIdiom == .phone
     static let iPad = UIDevice.current.userInterfaceIdiom == .pad
     
